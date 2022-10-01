f35aee458e45a2063a404777424347ce0f219111
BogdanLiovin <bogdan.win123@gmail.com>
2022-10-01 23:25:12 +0300

    my first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from './Header.module.css';
+
+const Header = () => {
+    return <header className={s.header}>
+        <img src='https://i.pinimg.com/736x/89/8e/9f/898e9f7750f3b6e652e4aad5680e7b85.jpg' />
+    </header>
+}
+
+export default Header;
